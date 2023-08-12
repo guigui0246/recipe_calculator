1a421850de3aa7cfbdf37cce6c2d174abed2a755
unknown <guigui0246@yahoo.com>
2023-08-12 16:54:37 +0200

    [ADD] add time recognition (not calculated for str=>float)

--- stime.py
+++ stime.py
@@ -1,5 +1,40 @@
+import re
+reg = r"(\d*d)?(\d*h)?(\d*m)?(\d*(?:\.|\,)?\d*s)?"
+pattern = re.compile(reg, re.IGNORECASE)
+
 def sec_to_time(time:float) -> str:
-    return str(time) + "secondes"
+    day = 0
+    hou = 0
+    min = 0
+    sec = time % 60
+    time -= sec
+    if (time > 0):
+        time /= 60
+        min = time % 60
+        time -= min
+        if (time > 0):
+            time /= 60
+            hou = time % 24
+            time -= hou
+            if (time > 0):
+                time /= 24
+                day = time
+    if (sec == min == hou == day == 0):
+        return "no time"
+    return f"{f'{day:g}d' if (day > 0) else ''}{f'{hou:g}h' if (hou > 0) else ''}{f'{min:g}m' if (min > 0) else ''}{f'{sec:g}s' if (sec > 0) else ''}"
 
 def time_to_sec(time:str) -> float:
-    return float("inf")
+    test = 0
+    value = float("inf")
+    for elem in pattern.findall(time):
+        if (elem == ('', '', '', '')):
+            continue
+        if (test > 0):
+            raise ValueError(f"Multiple time value at {time}")
+        test+=1
+    return value
+
+print(sec_to_time(14770754.15))
+print(time_to_sec(sec_to_time(14770754.15)))
+print(sec_to_time(time_to_sec("1d1h1m1s")))
+print(sec_to_time(time_to_sec("1d1h1s5m")))
