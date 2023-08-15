ad38cc947d7d7af8f3973a8a394b8ed46eb7c92a
unknown <guigui0246@yahoo.com>
2023-08-16 00:49:51 +0200

    [FIX] fix exception tests and warnings in stime.py

--- stime.py
+++ stime.py
@@ -3,13 +3,15 @@ from const import HOUR_STRING
 from const import MINUTE_STRING
 from const import SECONDE_STRING
 import re
-reg = f"(\d*{DAY_STRING})?(\d*{HOUR_STRING})?(\d*{MINUTE_STRING})?(\d*(?:\.|\,)?\d*{SECONDE_STRING})?"
+reg = f"(\\d*{DAY_STRING})?(\\d*{HOUR_STRING})?(\\d*{MINUTE_STRING})?(\\d*(?:\\.|\\,)?\\d*{SECONDE_STRING})?"
 pattern = re.compile(reg, re.IGNORECASE)
 from const import SECONDES_PER_MINUTES
 from const import MINUTES_PER_HOURS
 from const import HOURS_PER_DAY
 
 def sec_to_time(time:float) -> str:
+    if time < 0:
+        raise ValueError(f"Time cannot be negative")
     day = 0
     hou = 0
     min = 0
@@ -27,20 +29,29 @@ def sec_to_time(time:float) -> str:
                 time /= HOURS_PER_DAY
                 day = time
     if (sec == min == hou == day == 0):
-        return "no time"
+        from const import NO_TIME_STRING
+        return NO_TIME_STRING
     return f"{f'{day:g}{DAY_STRING}' if (day > 0) else ''}{f'{hou:g}{HOUR_STRING}' if (hou > 0) else ''}{f'{min:g}{MINUTE_STRING}' if (min > 0) else ''}{f'{sec:g}{SECONDE_STRING}' if (sec > 0) else ''}"
 
 def time_to_sec(time:str) -> float:
     test = 0
     value = 0
+    if time.find("-") != -1:
+        raise ValueError("- has been found and negative time doesn't exist")
     for elem in pattern.findall(time):
         if (elem == ('', '', '', '')):
             continue
         if (test > 0):
             raise ValueError(f"Multiple time values at {time}\nThese values are :\n{pattern.findall(time)[:-1]}\n")
         test+=1
-        value += int(elem[0][:-len(DAY_STRING)])*HOURS_PER_DAY*MINUTES_PER_HOURS*SECONDES_PER_MINUTES if not elem[0] == '' else 0
-        value += int(elem[1][:-len(HOUR_STRING)])*MINUTES_PER_HOURS*SECONDES_PER_MINUTES if not elem[1] == '' else 0
-        value += int(elem[2][:-len(MINUTE_STRING)])*SECONDES_PER_MINUTES if not elem[2] == '' else 0
         value += float(elem[3][:-len(SECONDE_STRING)]) if not elem[3] == '' else 0
+        if value > SECONDES_PER_MINUTES:
+            raise ValueError("Too much secondes")
+        value += int(elem[2][:-len(MINUTE_STRING)])*SECONDES_PER_MINUTES if not elem[2] == '' else 0
+        if value > MINUTES_PER_HOURS * SECONDES_PER_MINUTES:
+            raise ValueError("Too much minutes")
+        value += int(elem[1][:-len(HOUR_STRING)])*MINUTES_PER_HOURS*SECONDES_PER_MINUTES if not elem[1] == '' else 0
+        if value > HOURS_PER_DAY * MINUTES_PER_HOURS * SECONDES_PER_MINUTES:
+            raise ValueError("Too much hours")
+        value += int(elem[0][:-len(DAY_STRING)])*HOURS_PER_DAY*MINUTES_PER_HOURS*SECONDES_PER_MINUTES if not elem[0] == '' else 0
     return value
--- test_stime.py
+++ test_stime.py
@@ -6,6 +6,7 @@ from const import SECONDE_STRING
 from const import SECONDES_PER_MINUTES
 from const import MINUTES_PER_HOURS
 from const import HOURS_PER_DAY
+from const import NO_TIME_STRING
 
 #
 #   TIME TO SEC (string to float)
@@ -45,6 +46,25 @@ def test_1_day_1_minute_to_sec():
 def test_1_day_1_hour_1_minute_1_sec_to_sec():
     assert time_to_sec(f"1{DAY_STRING}1{HOUR_STRING}1{MINUTE_STRING}1{SECONDE_STRING}") == SECONDES_PER_MINUTES * MINUTES_PER_HOURS * HOURS_PER_DAY + SECONDES_PER_MINUTES + SECONDES_PER_MINUTES * MINUTES_PER_HOURS + 1
 
+@pytest.mark.time_to_sec
+def test_0_to_sec():
+    assert time_to_sec("") == 0
+
+@pytest.mark.time_to_sec
+def test_neg_to_sec():
+    with pytest.raises(ValueError):
+        time_to_sec("-1h")
+
+@pytest.mark.time_to_sec
+def test_not_in_range_to_sec():
+    with pytest.raises(ValueError):
+        time_to_sec(f"{MINUTES_PER_HOURS + 15}m")
+
+@pytest.mark.time_to_sec
+def test_multiple_to_sec():
+    with pytest.raises(ValueError):
+        time_to_sec("1h3m2d1h")
+
 #
 #   SEC TO TIME (float to string)
 #
@@ -66,6 +86,15 @@ def test_1_day_to_time():
 @pytest.mark.sec_to_time
 def test_1_sec_to_time():
     assert sec_to_time(1) == f"1{SECONDE_STRING}"
+
+@pytest.mark.sec_to_time
+def test_0_to_time():
+    assert sec_to_time(0) == NO_TIME_STRING
+
+@pytest.mark.sec_to_time
+def test_neg_to_time():
+    with pytest.raises(ValueError):
+        sec_to_time(-1)
 
 @pytest.mark.sec_to_time
 def test_1515_sec_to_time():
