968b02b96a18e0159f6b1e47fa237a745669582a
unknown <guigui0246@yahoo.com>
2023-10-01 23:15:39 +0200

    [ADD] add input

--- print_clearable.py
+++ print_clearable.py
@@ -1,5 +1,7 @@
 original_print = print
-from sys import stdout
+original_input = input
+import io
+from sys import stdout, stdin, stderr
 from typing import Generic, Literal, TextIO, TypeVar
 TextIO = TypeVar('TextIO')
 class SupportsWrite(Generic[TextIO]):
@@ -10,7 +12,6 @@ PRINT_BUFFER:dict[SupportsWrite,str] = {}
 
 def print(*args, sep:str | None = " ", end: str | None = "\n", file: SupportsWrite[str] | None = None, flush: Literal[False] = False, **kwargs):
     global PRINT_BUFFER
-    global LAST_END
     if file == None:
         file = stdout
     string = ''
@@ -26,16 +27,41 @@ def print(*args, sep:str | None = " ", end: str | None = "\n", file: SupportsWri
         PRINT_BUFFER[file] += string
     else:
         PRINT_BUFFER[file] = string
-    ret = original_print(string, sep=None, end="", flush=flush, file=file)
+    return original_print(string, sep=None, end="", flush=flush, file=file)
+
+def input(__prompt, **kwargs):
+    global PRINT_BUFFER
+    string = ''
+    if isinstance(__prompt, str):
+        string += __prompt
+    else:
+        string += str(__prompt)
+    if stdout in PRINT_BUFFER.keys():
+        PRINT_BUFFER[stdout] += string
+    else:
+        PRINT_BUFFER[stdout] = string
+    try:
+        ret = original_input(string, **kwargs)
+    except EOFError:
+        return None
+    if stdin in PRINT_BUFFER.keys():
+        PRINT_BUFFER[stdin] += ret+"\n"
+    else:
+        PRINT_BUFFER[stdin] = ret+"\n"
     return ret
 
-def clear(file:SupportsWrite[str]|None=None):
+def clear(file:SupportsWrite[str]|None=None, remove_errors:bool=False):
     global PRINT_BUFFER
     if file == None:
         file = stdout
     if not file in PRINT_BUFFER.keys():
         return
-    original_print("\033[F\033[K\r" * PRINT_BUFFER[file].count("\n"), file=file, end="")
+    try:
+        original_print("\033[F\033[K\r" * PRINT_BUFFER[file].count("\n"), file=file, end="")
+    except io.UnsupportedOperation:
+        original_print("\033[F\033[K\r" * PRINT_BUFFER[file].count("\n"), file=stdout, end="")
+        if not remove_errors:
+            original_print(RuntimeWarning("File could not be written for clear! Using stdout."), file=stderr)
     del PRINT_BUFFER[file]
 
 def clear_cache(file:SupportsWrite[str]|None=None, all:bool=False):
