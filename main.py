63ffef36f8d593180992054dc0f27fa962ac733f
unknown <guigui0246@yahoo.com>
2023-10-01 23:15:53 +0200

    [ADD] add ressource asking

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from loading import Loading
 LOAD = Loading()
 NUMBER_OF_LOADING_STEP = 3
@@ -11,15 +12,21 @@ from loader import get_inv
 inventory = get_inv()
 LOAD.valeur = 100/NUMBER_OF_LOADING_STEP * 3
 
-print(recipes, crafters, inventory, sep="\n")
-
 def find_best_recipe(ressource:str, quantity:int=1):
     possible_recipes = []
     for r in recipes:
         if r.produce(ressource):
             possible_recipes.append(r)
+    print(possible_recipes)
     #Add the crafters results to it
     #Find the fastest
     #Calculate the best recipe
 
-print("\n\n\n\n", recipes[0].infos())
+from print_clearable import print, clear, input
+ressource = input("Ressource to obtain:\n")
+clear(sys.stdout)
+clear(sys.stdin, True)
+print(find_best_recipe(ressource))
+import time
+time.sleep(1)
+clear()
