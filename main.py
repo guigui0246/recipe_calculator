ee07e889cacf283d08f756d01b913c6f5e8e55d8
unknown <guigui0246@yahoo.com>
2023-10-01 03:23:56 +0200

    [ADD] add loading to loading

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from loading import Loading
+LOAD = Loading()
+NUMBER_OF_LOADING_STEP = 3
 from loader import get_recipes
 recipes = get_recipes()
+LOAD.valeur = 100/NUMBER_OF_LOADING_STEP * 1
 from loader import get_crafter
 crafters = get_crafter()
+LOAD.valeur = 100/NUMBER_OF_LOADING_STEP * 2
 from loader import get_inv
 inventory = get_inv()
+LOAD.valeur = 100/NUMBER_OF_LOADING_STEP * 3
 
 print(recipes, crafters, inventory, sep="\n")
 
