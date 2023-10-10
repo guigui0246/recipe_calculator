5fdad6301655cc8798c826eadade71ec85509aac
unknown <guigui0246@yahoo.com>
2023-10-10 23:32:15 +0200

    [ADD] add conftest test

--- test_debug_logging.py
+++ test_debug_logging.py
@@ -2,6 +2,10 @@ import pytest
 import debug
 import os
 
+#
+# Debug
+#
+
 @pytest.mark.usefixtures("tmpdir", "monkeypatch")
 @pytest.hookimpl(tryfirst=True)
 @pytest.mark.debug
@@ -11,3 +15,35 @@ def test_log(tmpdir, monkeypatch):
     with open(os.path.join(".", "debug.log")) as f:
         txt = f.read()
     assert txt=="\"\"\" This is a test log \"\"\"\n"
+
+#
+# conftest.py
+#
+
+@pytest.hookimpl(tryfirst=True)
+@pytest.mark.debug
+def test_conftest_success():
+    class It():
+        def __init__(self) -> None:
+            self.data = "Data"
+        def clear(self) -> None:
+            del self.data
+    import conftest
+    conftest.Fail = False
+    item = It()
+    conftest.pytest_collection_modifyitems(None, item)
+    assert hasattr(item, "data")
+
+@pytest.hookimpl(tryfirst=True)
+@pytest.mark.debug
+def test_conftest_fail():
+    class It():
+        def __init__(self) -> None:
+            self.data = "Data"
+        def clear(self) -> None:
+            del self.data
+    import conftest
+    conftest.Fail = True
+    item = It()
+    conftest.pytest_collection_modifyitems(None, item)
+    assert not hasattr(item, "data")
